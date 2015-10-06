365442bcf9d65f76855580b523d0ed03efdb810e
Donavin Northrup <dnorthrup25@gmail.com>
2015-10-06 02:37:24 +0000

    Checkpoint 41 Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,9 @@
    def confirm
    end
    
+   def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
+   
  end
