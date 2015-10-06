6c6dde1137020cbf1b2ad2068ea41c71b688e6e2
Donavin Northrup <dnorthrup25@gmail.com>
2015-10-06 20:45:37 +0000

    Test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Comment, type: :model do
     let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
     let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
 
+
     it "should respond to body" do
       expect(comment).to respond_to(:body)
     end
