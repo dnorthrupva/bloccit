7116fcdae8645abb8211fc4ec3d1c1b717427da9
Donavin Northrup <dnorthrup25@gmail.com>
2015-10-07 00:21:50 +0000

    Pre-assignment Checkpoint 44

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151006200852) do
+ActiveRecord::Schema.define(version: 20151007001125) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 20151006200852) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -71,9 +71,18 @@ end
     )
 end
 
- user = User.first
- user.update_attributes!(
-   email: 'dnorthrupva@gmail.com', 
+ # Create an admin
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
 
