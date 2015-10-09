1b1287b809014b5fb76b01d68b97da28d26d1ed5
Donavin Northrup <dnorthrup25@gmail.com>
2015-10-09 20:50:24 +0000

    Checkpoint Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,11 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
   has_one :rating
-   has_many :comments, dependent: :destroy
-   has_many :votes, dependent: :destroy
-   has_many :labelings, as: :labelable
-   has_many :labels, through: :labelings
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
+  has_many :labelings, as: :labelable
+  has_many :labels, through: :labelings
+  after_create :create_vote
     
     default_scope { order('rank DESC') }
     scope :ordered_by_title, -> { order(:title)  }
@@ -33,4 +34,10 @@ class Post < ActiveRecord::Base
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
    end
+   
+   private
+   
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
 end
