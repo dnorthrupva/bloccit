2fc01259da5e208ccf05b5da6bc1d246805e03bd
Donavin Northrup <dnorthrup25@gmail.com>
2015-10-12 19:07:01 +0000

    Pre-assignment Checkpoint 51

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,14 +3,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   
   after_create :send_favorite_emails
-
+  validates :body, length: { minimum: 5 }, presence: true
+  validates :user, presence: true
+  default_scope { order('updated_at DESC') }
+  
   private
-  def send_favorite_emails
+   def send_favorite_emails
     post.favorites.each do |favorite|
       FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
     end
-  end
- 
-  validates :body, length: { minimum: 5 }, presence: true
-  validates :user, presence: true
+   end
 end
